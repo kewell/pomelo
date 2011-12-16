39da1ecff8dff453c4cf25213882abf00bf3b285
kewell0101@gmail.com <kewell0101@gmail.com@944a31ab-0be3-294b-bc57-fb793cc5cc4a>
2011-12-16 08:07:35 +0000

    svnrev test

---
+++ DEV00-COMMON/svnrev.c
@@ -0,0 +1,27 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2011 R&D of San Fran Electronics Co., LTD  
+ *                  All rights reserved.
+ *
+ *       Filename:  svnrev.c
+ *    Description:  This file 
+ *                 
+ *        Version:  1.0.0(12/16/2011~)
+ *         Author:  WENJING <WENJIGN0101@GMAIL.COM>
+ *      ChangeLog:  1, Release initial version on "12/16/2011 04:10:19 PM"
+ *                 
+ ********************************************************************************/
+
+#include "svnrev.h"
+/********************************************************************************
+ *  Description:
+ *   Input Args:
+ *  Output Args:
+ * Return Value:
+ ********************************************************************************/
+int main (int argc, char **argv)
+{
+    printf("SVN_REV=%s, SVN_REVDATE=%s\n", SVN_REV, SVN_REVDATE);
+
+    return 0;
+} /* ----- End of main() ----- */
+
---
+++ DEV00-COMMON/svnrev.h
@@ -0,0 +1,15 @@
+/********************************************************************************
+ *      Copyright:  (C) 2011 R&D of San Fran Electronics Co., LTD  
+ *                  All rights reserved.
+ *
+ *       Filename:  svnrev.h
+ *    Description:  This head file 
+ *
+ *        Version:  1.0.0(12/16/2011~)
+ *         Author:  WENJING <WENJIGN0101@GMAIL.COM>
+ *      ChangeLog:  1, Release initial version on "12/16/2011 04:11:45 PM"
+ *                 
+ ********************************************************************************/
+
+#define SVN_REV ""
+#define SVN_REVDATE ""
