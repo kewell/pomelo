82e77307605e6552cdef302cafbd7007ecd7a230
kewell0101 <kewell0101@gmail.com>
2014-08-28 13:38:49 +0800

    make binarys more efficiently

--- DEV05-SAM9260/2014/kernel/build.sh
+++ DEV05-SAM9260/2014/kernel/build.sh
@@ -1,5 +1,6 @@
 
 wget -c https://www.kernel.org/pub/linux/kernel/v2.6/linux-2.6.22.tar.xz
 cd linux-2.6.22
-patch -p1 < ../linux-2.6.22_9260.patch
+#patch -p1 < ../linux-2.6.22_9260.patch
+zcat ../linux-2.6.22_9260.patch | patch -p1
 make uImage
